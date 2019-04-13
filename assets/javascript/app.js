48a81db44de0e076a73afc1602d76d320fd4fc88
krekorian@gmail.com <krekorian@gmail.com>
2019-04-12 17:48:25 -0700

    04122019_2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 
         $('#startButton').hide();
         $('#submitButton').append('<button type="button" class="submit-button btn btn-secondary btn-lg ">Submit</button>');
+        $('#timing-frame').append('<h2>Time Remaining: <span id="timer">10</span> Seconds</h2>');
         timer = setInterval(timeIt, 1000);
     });
 
@@ -16,7 +17,17 @@ $(document).ready(function () {
 
         if (timeLeft === 0) {
             clearInterval(timer);
+            getResults();
         }
+    };
+
+    //function to show results
+    function getResults() {
+        $('#submitButton').hide();
+        $('#timing-frame').hide();
+        $('#result-page').append('<h2> Corrent: ' + '</h2>');
+        $('#result-page').append('<h2> Incorrent: ' + '</h2>');
+        $('#result-page').append('<h2> Unanswered: ' + '</h2>');
     };
 
 });
