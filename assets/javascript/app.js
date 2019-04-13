9d66d77f7f03d8e191896c63899107cf432b932c
krekorian@gmail.com <krekorian@gmail.com>
2019-04-12 20:51:56 -0700

    04122019_4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var arrayElement;
     var j = 1;
     var v;
-    var questions = [{ question1: "Who discovered penicillin?", answer1: "Edison", answer2: "Fleming", answer3: "Tesla", answer4: "Jenner" }, { question2: "Who wrote Julius Caesar, Macbeth and Hamlet?", answer1: "Shakespeare", answer2: "Charles Dickens", answer3: "R.J. Wells", answer4: "Lord Bayron" }, { question3: "Where was Lope de Vega born?", answer1: "Paris", answer2: "Barcelona", answer3: "Buenes Aires", answer4: "Madrid" }, { question4: "When did the First World War start?", answer1: "1914", answer2: "1915", answer3: "1916", answer4: "1913" }, { question5: "What does the roman numeral C represent?", answer1: "5", answer2: "10", answer3: "50", answer4: "100" }, { question6: "Who painted the Mona Lisa?", answer1: "Raphael", answer2: "Da Vinci", answer3: "Michael Angelo", answer4: "Donatello" }];
+    var questions = [{ question1: "Who discovered penicillin?", answer1: "Edison", answer2: "Fleming", answer3: "Tesla", answer4: "Jenner" }, { question2: "Who wrote Julius Caesar, Macbeth and Hamlet?", answer1: "Shakespeare", answer2: "Charles Dickens", answer3: "R.J. Wells", answer4: "Lord Bayron" }, { question3: "Where was Lope de Vega born?", answer1: "Paris", answer2: "Barcelona", answer3: "Buenes Aires", answer4: "Madrid" }, { question4: "When did the First World War start?", answer1: "1914", answer2: "1915", answer3: "1916", answer4: "1913" }, { question5: "What does the roman numeral C represent?", answer1: "5", answer2: "10", answer3: "50", answer4: "100" }, { question6: "Who painted the Mona Lisa?", answer1: "Raphael", answer2: "Da Vinci", answer3: "Michael Angelo", answer4: "Donatello" }, { question7: "When did the American Civil War end?", answer1: "1864", answer2: "1865", answer3: "1866", answer4: "1867" }];
     //console.log(questions.length);
     //arrayElement = questions[0];
     // console.log(arrayElement["question" + j]);
@@ -15,6 +15,8 @@ $(document).ready(function () {
         $('#startButton').hide();
         $('#submitButton').append('<button type="button" class="submit-button btn btn-secondary btn-lg ">Submit</button>');
         $('#timing-frame').append('<h2>Time Remaining: <span id="timer">10</span> Seconds</h2>');
+        $('#main-frame').removeClass("main-content-frame");
+        $('#main-frame').addClass("main-content-frame1");
         timer = setInterval(timeIt, 1000);
         displayQuestions();
     });
